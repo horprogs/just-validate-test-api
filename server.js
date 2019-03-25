a07747c368ae9c1a4bada8e032dec8e6bfcb2855
Perepecho Georgy <g.perepecho@semrush.com>
2019-03-25 18:18:51 +0300

    move from just-validate package

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 7777;
+
+app.set('port', port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
+app.get('/check-correct', function(req, res) {
+    if (req.query.login === 'testLogin') {
+        res.send('OK');
+        return;
+    }
+
+    if (req.query.email === 'ok@test.com') {
+        res.send('OK');
+        return;
+    }
+    res.send('false');
+});
+
+app.post('/submit', function(req, res) {
+    res.send('OK');
+});
+
+app.get('/check-wrong', function(req, res) {
+    res.send('false');
+});
+
+app.listen(app.get('port'));
